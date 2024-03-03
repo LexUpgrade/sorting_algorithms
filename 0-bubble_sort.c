6e82bcbfbf8be17dbb0d90a8de494c3257e3b3a6
Lex9jar <alexanderedim89@gmail.com>
2024-03-03 17:11:28 +0100

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,6 +13,9 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int tmp;
 
+	if (array == NULL || size <= 0)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
