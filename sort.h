00a2d141d90527c4df43512d435b7852ad673c2c
Lex9jar <alexanderedim89@gmail.com>
2024-03-05 23:10:37 +0100

    Header file

--- sort.h
+++ sort.h
@@ -25,6 +25,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 /* - - - - - - UTILS - - - - - - */
 void swap(int *num1, int *num2);
