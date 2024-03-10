d4189bda2fbe8df918097b57b23b2746a7088656
Lex9jar <alexanderedim89@gmail.com>
2024-03-10 03:17:05 +0100

    Header file

--- sort.h
+++ sort.h
@@ -3,6 +3,21 @@
 
 #include <stddef.h>
 
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
 /**
  * struct listint_s - Doubly linked list node
  *
